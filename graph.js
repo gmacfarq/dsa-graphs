3cf14d856500bfe5a28e93a290813faa18304e33
Russell Jones <justyrones@gmail.com>
2023-06-15 15:04:10 -0600

    bfs done

--- graph.js
+++ graph.js
@@ -63,7 +63,42 @@ class Graph {
   }
 
   /** traverse graph with BDS and returns array of Node values */
-  breadthFirstSearch(start) { }
+  breadthFirstSearch(start, seen =[start], values=[start.value], nextLayer=[]) {
+    // if (!start) return empty
+    if(!start && start !== null) return [];
+    let nextNextLayer = [];
+
+    //if start is NOT null,
+        //loop through start.adjacent, adding each adjacent node that isnt in the seen
+      //array to the seen array and add each of those new adjacent nodes's values
+      //to the values array, and add all of their adjacsents to the nextLayer
+    if (start){
+      for (let adj of start.adjacent){
+          values.push(adj.value);
+          seen.push(adj);
+          adj.adjacent.forEach(node=> nextLayer.push(node));
+      }
+     return this.breadthFirstSearch(null, seen, values, nextLayer)
+    }
+
+    //if start IS null,   <---dont even need this if
+    // loop through currentLayer, see if current node is alrready in seen, if not,
+    //add its value to the values array, add it to the seen array, and add its
+    // adjascents to the next layer array
+    //if nextLayer.length === 0 , then return values
+    // -----return recursive call start=null, [...seen,...nextLayer], seen, values
+    for (let node of nextLayer){
+      if(!seen.includes(node)){
+        values.push(node.value);
+        seen.push(node);
+        node.adjacent.forEach(el=>{
+        nextNextLayer.push(el);
+        })
+      }
+    }
+    return nextLayer.length ?  this.breadthFirstSearch(null, seen, values, nextNextLayer)
+                            :  values;
+  }
 
   /** find the distance of the shortest path from the start vertex to the end vertex */
   distanceOfShortestPath(start, end) { }
