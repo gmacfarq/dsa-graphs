9011ffc31d8d9014351797e8f4cc3e54eb87fcb9
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-06-15 11:57:36 -0700

    dsa-graphs add,remove,dFS

---
+++ graph.js
@@ -0,0 +1,72 @@
+/** Node class for graph. */
+
+class Node {
+  constructor(value, adjacent = new Set()) {
+    this.value = value;
+    this.adjacent = adjacent;
+  }
+}
+
+
+/** Graph class. */
+
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+  }
+
+  /** add Node instance and add it to nodes property on graph. */
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
+
+  /** add array of new Node instances and adds to them to nodes property. */
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.nodes.add(vertex);
+    }
+  }
+
+  /** add edge between vertices v1,v2 */
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
+
+  /** remove edge between vertices v1,v2 */
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
+
+  /** remove vertex from graph:
+   *
+   * - remove it from nodes property of graph
+   * - update any adjacency lists using that vertex
+   */
+  removeVertex(vertex) {
+    this.nodes.delete(vertex);
+    for (let adjacentVertex of vertex.adjacent) {
+      adjacentVertex.adjacent.delete(vertex);
+    }
+  }
+
+  /** traverse graph with DFS and returns array of Node values */
+  depthFirstSearch(start, seen=[start.value]) {
+    for (let neighbor of start.adjacent) {
+      if (!seen.includes(neighbor.value)) {
+        seen.push(neighbor.value);
+        seen = this.depthFirstSearch(neighbor, seen);
+      }
+    }
+    return seen;
+  }
+
+  /** traverse graph with BDS and returns array of Node values */
+  breadthFirstSearch(start) { }
+
+  /** find the distance of the shortest path from the start vertex to the end vertex */
+  distanceOfShortestPath(start, end) { }
+}
+
+module.exports = { Graph, Node };
